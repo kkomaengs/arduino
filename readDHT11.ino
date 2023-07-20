d626f412ec4885137bccd3817246ded486b7f212
kkomaengs <139943366+kkomaengs@users.noreply.github.com>
2023-07-20 11:49:25 +0900

    Update readDHT11.ino

--- readDHT11.ino
+++ readDHT11.ino
@@ -1,10 +1,10 @@
 byte humidity = 0;
 #include <DFRobot_DHT11.h>
 DFRobot_DHT11 DHT;
-#define DHT11_PIN 13 //아두이노 핀 10번으로 정의하겠다.
+#define DHT11_PIN 13 //아두이노 핀 13번으로 정의하겠다.
 
 void setup(){
-  Serial.begin(9600); //컴퓨터와 통신을 115200속도로 하겠다.)
+  Serial.begin(9600); //컴퓨터와 통신을 9600속도로 하겠다.)
 }
 
 void loop(){
